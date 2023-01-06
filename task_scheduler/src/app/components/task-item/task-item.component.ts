346922d39f971a073b50b08af7a47a3753fb9d50
Roger Madhu <rogermadhu.dev@gmail.com>
2023-01-05 19:51:19 -0500

    Task Scheduler Completed

---
+++ task_scheduler/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input() task: Task;
+  faTimes = faTimes;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  
+  constructor() {
+  }
+
+  onDelete(task:Task) {
+    this.onDeleteTask.emit(task);
+    // console.log(task.text);
+  }
+
+  onToggle(task:Task) {
+    this.onToggleReminder.emit(task);
+  }
+}
